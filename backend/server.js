ddfec31c4daf9bd34424830323c6820bd2d8d1c5
Matthias <matthias.mews.pro@gmail.com>
2023-06-30 16:06:46 +0200

    Middleware + destination routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 8081
+const port = 8081;
 
 
-app.get("/exoplanet", (req, res) =>{
-    res.json({ message: "Voici les données" });
-});
+// Middleware pour traiter les données
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// récuparation routes
+app.use("/exoplanet", require ("./routes/exoplanet.routes"))
 
 // Run server 
 app.listen(port, () => console.log(" Server start at " + port))
