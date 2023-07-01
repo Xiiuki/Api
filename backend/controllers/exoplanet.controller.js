72cecdd1d7bb61ab549831fda5fdc97d471c64c4
Matthias <matthias.mews.pro@gmail.com>
2023-07-01 20:57:57 +0200

    controller exoplanets for getOne/getAll/update/delete

---
+++ backend/controllers/exoplanet.controller.js
@@ -0,0 +1,69 @@
+const exoplanetModel = require ('../models/exoplanet.model')
+
+//POST
+module.exports.setExoplanetsPost = async (req, res, next) => {
+    delete req.body._id;
+    const exoplanet = new exoplanetModel({
+        ...req.body
+    });
+    exoplanet.save()
+        .then(() => res.status(201).json({ message: "Object is saved !"}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+//GET ALL
+module.exports.getExoplanetsAll = async (req, res, next) => {
+    exoplanetModel.find()
+        .then(exoplanetshema => res.status(201).json(exoplanetshema))
+        .catch(error => res.status(400).json({ error }));
+};
+
+//GET ONE
+module.exports.getExoplanetsOne = async (req, res, next) => {
+    try {
+        const exoplanet = await exoplanetModel.findOne({ planet_name: req.params.planet_name });
+
+        if (!exoplanet) {
+            return res.status(400).json({ message: "This exoplanet doesn't exist!" });
+        }
+
+        res.status(200).json(exoplanet);
+
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+ 
+
+//UPDATE
+module.exports.updateExoplanets = async (req, res) => {
+    try {
+        const exoplanet = await exoplanetModel.findOne({ planet_name: req.params.planet_name });
+
+        if (!exoplanet) {
+            return res.status(400).json({ message: "This exoplanet doesn't exist!" });
+        }
+
+        exoplanet.set(req.body);
+        await exoplanet.save();
+
+        res.status(200).json({ message: "This exoplanet has been updated successfully!" });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+};
+
+//DELETE
+module.exports.deleteExoplanets = async (req, res) => {
+    const exoplanet = await exoplanetModel.findOne({ planet_name: req.params.planet_name });
+
+    if (!exoplanet) {
+        return res.status(400).json({ message: "This exoplanet doesn't exist!" });
+    }
+
+    await exoplanet.deleteOne({ _id: exoplanet._id });
+    
+    res.status(200).json({ message: req.params.planet_name + " has been deleted successfully!" });
+
+    
+};
