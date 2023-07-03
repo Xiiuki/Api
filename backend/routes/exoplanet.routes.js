c30e20c7b68674df4963107840bb382c1b4d6b8b
Matthias <matthias.mews.pro@gmail.com>
2023-07-03 22:50:19 +0200

    Update routes

--- backend/routes/exoplanet.routes.js
+++ backend/routes/exoplanet.routes.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const { setExoplanetsPost, getExoplanetsAll, updateExoplanets, getExoplanetsOne, deleteExoplanets } = require('../controllers/exoplanet.controller');
 const router = express.Router();
 
 
 //GET
-router.get("/", (req, res) => {
-    res.json({ message: "test" });
-});
+router.get ("/one/:planet_name", getExoplanetsOne)
+
+//GET ALL
+router.get('/', getExoplanetsAll)
 
 //POST
+router.post("/add", setExoplanetsPost);
 
 //UPDATE
+router.put("/update/:planet_name", updateExoplanets)
 
 //DELETE
+router.delete("/delete/:planet_name", deleteExoplanets)
+//
 module.exports = router; 
