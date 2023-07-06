5e30be292c7be11f295b1201e1e26c68abad1af6
Matthias <matthias.mews.pro@gmail.com>
2023-07-06 22:57:16 +0200

    ajout des routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 8081;
+const port = 5000;
 const connectDB = require("./db/database");
 const dotenv = require("dotenv").config();
+const exoplanetRoutes = require("./routes/exoplanet.routes");
 
-
-// Connection MongoDB
+// Connection MongoDBnpm 
 connectDB()
 
 // Middleware pour traiter les données
@@ -13,7 +13,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // récuparation routes
-app.use("/exoplanet", require ("./routes/exoplanet.routes"))
+
+//GET ONE
+app.use("/exoplanet/:planet_name", exoplanetRoutes)
+
+//GET ALL
+app.use("/exoplanet", exoplanetRoutes)
+
+//Post
+app.use("/exoplanet/add", exoplanetRoutes)
+
+//Update
+app.use("/exoplanet/update/:planet_name", exoplanetRoutes)
+
+//Delete
+app.use("/exoplanet/delete/:planet_name", exoplanetRoutes)
 
 // Run server 
 app.listen(port, () => console.log(" Server start at " + port))
